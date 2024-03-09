a511f15a5a3ba6d78096842a2a29c6ed156ebb21
Addin Malik <addinmalik@gmail.com>
2024-03-09 10:58:55 +0700

    INITIAL COMMIT

---
+++ src/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Addinmalik\Tglindo;
+
+class Main {
+    public function __construct() {
+        echo "Main class has been initialized";
+    }
+
+    public function sayHello() {
+        echo "Hello from Main class";
+    }
+}
